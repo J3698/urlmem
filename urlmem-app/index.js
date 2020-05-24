a54aed49562770253224d44ca060b715119852b8
Antioch Sanders <antiochsanders@gmail.com>
2020-05-24 16:36:35 -0400

    Add database capabilities to app

--- urlmem-app/index.js
+++ urlmem-app/index.js
@@ -2,6 +2,7 @@ const urlmem = require('urlmem');
 const express = require('express'); // import express
 const app = express();
 const path = require('path');
+const db = require('./queries')
 
 const shortenedLength = 7;
 
@@ -14,26 +15,31 @@ const port = process.env.PORT || 5000;
 
 // output whether the app runs or errors
 app.listen(port, function (err) {
-  if (err)
+  if (err) {
     console.log(err);
-  else
+  } else {
     console.log("App running");
+  }
 });
 
-const longToCustomMappings = {};
-const longToRandomMappings = {};
-const shortToLongMappings = {};
-const usedWords = new Set();
-
-function getNextUnusedWord() {
+/*
+ * Return a random word that is not currently in use.
+ *
+ * Note: It is possible that the word will become 'used' immediately after
+ * this function returns.
+ */
+async function getNextUnusedWord() {
   let word = urlmem.randomWord(shortenedLength);
-  while (usedWords.has(word)) {
-    word = urlmem.randomWord(shortenedLength);
+  if (await db.isShortUrlUsed(word)) {
+    return await getNextUnusedWord();
+  } else {
+    return word;
   }
-  usedWords.add(word);
-  return word;
 }
 
+/*
+ * Add 'http://' to URLs that are missing the http and https prefix.
+ */
 function qualify(longUrl) {
   if (longUrl.startsWith('http://') ||
       longUrl.startsWith('https://')) {
@@ -42,58 +48,88 @@ function qualify(longUrl) {
   return 'http://' + longUrl
 }
 
-function customWordUnavailable(longUrl, customUrl) {
-  if (unavailable = usedWords.has(customUrl) &&
-      !(customUrl in longToCustomMappings[longUrl])) {
-    // already in use
-    return false;
-  } else if (customUrl == "" || customUrl == "index" ||
-             customUrl == "index.htm" || customUrl == "index.html") {
+/*
+ * Return true if and only if a customUrl is not reserved or currently in use.
+ *
+ * Note: It is possible that the word will become 'used' immediately after
+ * this function returns.
+ */
+async function customUrlAvailable(customUrl) {
+  if (customUrl == "" || customUrl == "index" ||
+      customUrl == "index.htm" || customUrl == "index.html") {
     // reserved for home page
     return false;
+  } else {
+    return !(await db.isShortUrlUsed(customUrl));
   }
-  return true;
 }
 
-function createCustomMapping(longUrl, customUrl) {
-  if (!(longUrl in longToCustomMappings)) {
-    longToCustomMappings[longUrl] = []
+/*
+ * Get the random shorturl associated with a longurl, creating one first if none
+ * exists, and then calls callback(randomurl, longurl).
+ */
+async function getOrCreateRandomMapping(longUrl) {
+  // test if random shortening exists
+  let randomUrl = await db.getRandomShortening(longUrl);
+  if (randomUrl !== null) {
+    console.log("got random shortening");
+    return randomUrl;
+  } else {
+    console.log("no shortening found");
+    // make new random shortening if it does not exist
+    let randomWord = await getNextUnusedWord();
+    console.log("got rand word");
+    randomUrl = await db.addShortening(randomWord, longUrl, true);
+    console.log("tried adding shortening");
+    // handle race condition
+    if (randomUrl === null) {
+      console.log("handling race");
+      randomUrl = await getOrCreateRandomMapping(longUrl);
+    }
   }
-  longToCustomMappings[longUrl].push(customUrl);
-
-  shortToLongMappings[customUrl] = longUrl;
-  usedWords.add(customUrl);
-
-  return customUrl
+  console.log("url", randomUrl);
+  return randomUrl;
 }
 
-function createRandomMapping(longUrl) {
-  let word = getNextUnusedWord();
-  longToRandomMappings[longUrl] = word;
-  shortToLongMappings[word] = longUrl;
+/*
+ * Create a custom shortening if it does not exist and is valid, return null
+ * if the shortening does not exist and is not valid.
+ */
+async function createCustomMapping(customUrl, longUrl) {
+  if (!customUrlAvailable(customUrl)) {
+    return null;
+  }
+  return await db.addShortening(randomUrl, longUrl, false);
 }
 
-// GET route for custom URLs
+/*
+ * Respond with the customurl if it is valid and exists or can be created,
+ * otherwise respond with a null shortening.
+ */
 app.get('/custom', (req, res) => {
   // sanitize input
   if (typeof (req.query.longUrl) !== "string" ||
       typeof (req.query.customUrl) !== "string") {
     res.send({"shortened": null});
-    return
   }
 
   let longUrl = qualify(req.query.longUrl);
   let customUrl = req.query.customUrl;
-
-  if (customWordUnavailable(longUrl, customUrl)) {
-    res.send({"shortened": null});
-  } else {
-    customUrl = createCustomMapping(longUrl, customUrl);
-    res.send({"shortened": customUrl});
-  }
+  db.addShortening(customUrl, longUrl, false).then((resp) => {
+    if (resp === null) {
+      console.log("unavailable");
+      res.send({"shortened": null});
+    } else {
+      console.log(customUrl, "available");
+      res.send({"shortened": customUrl});
+    }
+  });
 });
 
-// GET route for random Urls
+/*
+ * Respond with a random shorturl, creates a new one only if a random shorturl
+ * does not already exist.
+ */
 app.get('/random', (req, res) => {
   // sanitize input
   if (typeof (req.query.longUrl) !== "string") {
@@ -102,22 +138,30 @@ app.get('/random', (req, res) => {
   }
 
   const longUrl = qualify(req.query.longUrl);
-  if (longUrl in longToRandomMappings) {
-    res.send({"shortened": longToRandomMappings[longUrl]});
-  } else {
-    createRandomMapping(longUrl);
-    res.send({"shortened": word});
-  }
+  console.log("qualified");
+  getOrCreateRandomMapping(longUrl).then((randomUrl) => {
+    res.send({"shortened": randomUrl});
+    console.log(randomUrl);
+    console.log("response sent");
+  });
 });
 
+/*
+ * Respond with the homepage.
+ */
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
+/*
+ * Redirect valid shorturl to associated longurl, otherwise redirect home.
+ */
 app.get('/[a-z]*', (req, res) => {
   let shortenedUrl = req.originalUrl.slice(1);
-  if (shortToLongMappings[shortenedUrl]) {
-    res.redirect(shortToLongMappings[shortenedUrl]);
-  } else {
-    res.redirect("/");
-  }
+  db.getLongUrl(shortenedUrl).then((longUrl) => {
+    if (longUrl !== null) {
+      res.redirect(longUrl);
+    } else {
+      res.redirect("/");
+    }
+  });
 });
 
---
+++ urlmem-app/queries.js
@@ -0,0 +1,91 @@
+const assert = require('assert');
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+client.connect(err => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
+  }
+});
+
+let isShortUrlUsedQuery = 'SELECT * FROM shortenings WHERE shorturl = $1';
+let getLongUrlQuery = 'SELECT (longurl) FROM shortenings WHERE shorturl = $1';
+let getRandomShorteningQuery = 'SELECT (shorturl) FROM shortenings WHERE longurl = $1 AND israndom = True';
+let addShorteningQuery = 'INSERT INTO shortenings (shorturl, longurl, israndom) VALUES ($1, $2, $3)';
+
+/*
+ * Returns whether a short url is taken.
+ *
+ * May throw other db errors.
+ */
+const isShortUrlUsed = async (shortUrl) => {
+  let res = await client.query(isShortUrlUsedQuery, [shortUrl]);
+  assert(res.rows.length < 2, "short url has multiple rows!");
+  return res.rows.length !== 0;
+}
+
+/*
+ * Returns longurl of shortening if it exists, otherwise null.
+ *
+ * May throw other db errors.
+ */
+const getLongUrl = async (shortUrl) => {
+  let res = await client.query(getLongUrlQuery, [shortUrl]);
+  assert(res.rows.length < 2, "short url has multiple rows!");
+  if (res.rows.length === 1) {
+    return res.rows[0]['longurl'];
+  }
+  return null;
+}
+
+/*
+ * Returns shorturl of random shortening if it exists, otherwise null.
+ *
+ * May throw other db errors.
+ */
+const getRandomShortening = async (longUrl) => {
+  let res = await client.query(getRandomShorteningQuery, [longUrl]);
+  assert(res.rows.length < 2, "random long shortening has multiple rows!");
+  if (res.rows.length === 1) {
+    return res.rows[0]['shorturl'];
+  }
+  return null;
+}
+
+/*
+ * Create a shortening and return the shorturl if successful. Returns null if
+ * the shorturl already exists and is not the requested shortening, or if the
+ * longurl already has a random shortening and this shortening is also random.
+ *
+ * May throw other db errors.
+ */
+const addShortening = async (shortUrl, longUrl, isRandom) => {
+  try {
+    await client.query(addShorteningQuery, [shortUrl, longUrl, isRandom]);
+    return shortUrl;
+  } catch (e) {
+    if (e.constraint === 'shortenings_pkey') {
+        // shortUrl is taken, check if it's the same shortening
+        return (await getLongUrl(shortUrl)) === longUrl ? shortUrl : null;
+    } else if (e.constraint === 'random_shortening_constraint') {
+        // can't add two random urls for one longurl
+        return null;
+    }
+    // some other error
+    throw e;
+  }
+}
+
+module.exports = {
+  isShortUrlUsed,
+  getLongUrl,
+  getRandomShortening,
+  addShortening,
+}
